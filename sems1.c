8b8cefa1958e4945f313bfd4d6a10e8b1bdf79fe
star151204@gmail.com <star151204@gmail.com>
2024-05-27 12:47:27 +0700

    Добавилди программу для работы с семафорамии

---
+++ sems1.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int semid;
+	char pathname[] = "key.ipc";
+	key_t key;
+	struct sembuf mybuf;
+	if((key = ftok(pathname, 0))<0) {
+		printf("генерация не удалась\n");
+		exit(-1);
+	}
+	if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+		printf("Дескриптор массива семафора не получен\n");
+		exit(-1);
+	}
+	mybuf.sem_op = -1;
+	mybuf.sem_flg = 0;
+	mybuf.sem_num = 0;
+	if(semop(semid, &mybuf, 1) < 0) {
+		printf("не удалось выполнить операцию над семафором\n");
+		exit(-1);
+	}
+	printf("Условие разблокировки процесса выолнено\n");
+	return 0;
+}
